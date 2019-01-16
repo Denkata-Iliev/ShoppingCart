8c173070d5e48416e879a78a0dd842a149337e4d
Denkata-Iliev <denislav9711@gmail.com>
2019-01-16 16:20:13 +0200

    Renamed variables. Probable final version

--- src/Cart/Interface.java
+++ src/Cart/Interface.java
@@ -7,19 +7,19 @@ import java.text.DecimalFormat;
 
 public class Interface extends JFrame{
     private static DecimalFormat formatSum = new DecimalFormat("#.##");
-    private JLabel label = new JLabel("Продукти:");
-    private JComboBox<String> products = new JComboBox();
+    private JLabel labelProducts = new JLabel("Продукти:");
+    private JComboBox<String> productsComboBox = new JComboBox();
     private JLabel pricesLabel = new JLabel("Цени:");
     private JTextArea pricesTextArea = new JTextArea();
-    private JScrollPane productsPrices = new JScrollPane(pricesTextArea);
-    private JLabel userChoice = new JLabel("Вие избрахте:");
-    private JButton buy = new JButton("Buy");
-    private JButton remove = new JButton("Remove");
-    private JButton finish = new JButton("Finish");
-    private JButton removeAll = new JButton("Remove All");
-    private JList<String> chosenList = new JList<>();
-    private DefaultListModel<String> model = new DefaultListModel<>();
-    private JScrollPane listScrollPane = new JScrollPane(chosenList);
+    private JScrollPane pricesScrollPane = new JScrollPane(pricesTextArea);
+    private JLabel userChoiceLabel = new JLabel("Вие избрахте:");
+    private JButton buyButton = new JButton("Buy");
+    private JButton removeButton = new JButton("Remove");
+    private JButton finishButton = new JButton("Finish");
+    private JButton removeAllButton = new JButton("Remove All");
+    private JList<String> userChoiceList = new JList<>();
+    private DefaultListModel<String> modelList = new DefaultListModel<>();
+    private JScrollPane listScrollPane = new JScrollPane(userChoiceList);
 
     public Interface() {
         super("Shopping Cart");
@@ -29,34 +29,34 @@ public class Interface extends JFrame{
         Container container = this.getContentPane();
         container.setLayout(new GridLayout(5,2,2,2));
 
-        container.add(label);
-        container.add(products);
-        products.addItem("Ябълка");
-        products.addItem("Круша");
-        products.addItem("Ягода");
-        products.addItem("Кайсия");
-        products.addItem("Праскова");
-        products.addItem("Ананас");
-        products.addItem("Слива");
-        products.addItem("Портокал");
+        container.add(labelProducts);
+        container.add(productsComboBox);
+        productsComboBox.addItem("Ябълка");
+        productsComboBox.addItem("Круша");
+        productsComboBox.addItem("Ягода");
+        productsComboBox.addItem("Кайсия");
+        productsComboBox.addItem("Праскова");
+        productsComboBox.addItem("Ананас");
+        productsComboBox.addItem("Слива");
+        productsComboBox.addItem("Портокал");
 
         pricesTextArea.setText("Ябълки - 2.00лв\nКруши - 2.20лв\nЯгоди - 3.10лв\nКайсии - 2.30лв\nПраскови - 3.00лв\n"
                 + "Ананаси - 2.50лв\nСливи - 2.60лв\nПортокали - 2.10лв");
         container.add(pricesLabel);
-        container.add(productsPrices);
+        container.add(pricesScrollPane);
 
-        container.add(userChoice);
+        container.add(userChoiceLabel);
         container.add(listScrollPane);
-        chosenList.setModel(model);
+        userChoiceList.setModel(modelList);
 
-        container.add(buy);
-        buy.addActionListener(new buyEventListener());
-        container.add(remove);
-        remove.addActionListener(new removeEventListener());
-        container.add(finish);
-        finish.addActionListener(new finishEventListener());
-        container.add(removeAll);
-        removeAll.addActionListener(new removeAllEventListener());
+        container.add(buyButton);
+        buyButton.addActionListener(new buyEventListener());
+        container.add(removeButton);
+        removeButton.addActionListener(new removeEventListener());
+        container.add(finishButton);
+        finishButton.addActionListener(new finishEventListener());
+        container.add(removeAllButton);
+        removeAllButton.addActionListener(new removeAllEventListener());
     }
 
     private double sum = 0;
@@ -71,9 +71,9 @@ public class Interface extends JFrame{
             double pineapplePrice = 2.50;
             double plumPrice = 2.60;
             double orangePrice = 2.10;
-            String selectedItem = (String) products.getSelectedItem();
-            model.addElement(selectedItem);
-            chosenList.setSelectedIndex(0);
+            String selectedItem = (String) productsComboBox.getSelectedItem();
+            modelList.addElement(selectedItem);
+            userChoiceList.setSelectedIndex(0);
             switch (selectedItem) {
                 case "Ябълка": sum+= applePrice; break;
                 case "Круша": sum+= pearPrice; break;
@@ -98,11 +98,11 @@ public class Interface extends JFrame{
             double pineapplePrice = 2.50;
             double plumPrice = 2.60;
             double orangePrice = 2.10;
-            int chosenIndex = chosenList.getSelectedIndex();
-            String listSelectedItem = chosenList.getSelectedValue();
+            int chosenIndex = userChoiceList.getSelectedIndex();
+            String listSelectedItem = userChoiceList.getSelectedValue();
             if (chosenIndex >= 0) {
-                model.removeElementAt(chosenIndex);
-                chosenList.setSelectedIndex(0);
+                modelList.removeElementAt(chosenIndex);
+                userChoiceList.setSelectedIndex(0);
                 switch (listSelectedItem) {
                     case "Ябълка": sum-= applePrice; break;
                     case "Круша": sum-= pearPrice; break;
@@ -129,7 +129,7 @@ public class Interface extends JFrame{
 
     class removeAllEventListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            model.removeAllElements();
+            modelList.removeAllElements();
             sum -= sum;
         }
     }
